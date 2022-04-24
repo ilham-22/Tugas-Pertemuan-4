b098426e237dc6675617ceb8647d564faf8a317a
ilham <101658474+ilham-22@users.noreply.github.com>
2022-04-24 23:28:14 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : ILHAM</li>
+ <li>Nama Belakang : IAM</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> JALAN TOMANG UTARA 1 </li>
+ </ul>
+
+ <h4>Tempat Lahir, Tanggal Lahir</h4>
+ <ul type="none">
+ <li>Jakarta, 22 MEI 2000</li>
+ </ul>
+ <h4>MY HOBBY</h4>
+ <ul type="square">
+ <li>DUGEM</li>
+ <li>BILIARD</li>
+ </ul>
+</section>
---
+++ v_footer.php
@@ -0,0 +1,6 @@
+<footer>
+ <a href=”http://www.RAJAJUDI.com”>Raja JUDI </a>
+ </footer>
+ </div>
+</body>
+</html>
